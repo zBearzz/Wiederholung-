57498d8417a4b0edcf9eba24ed85dc6962545cb7
zBearzz <90605099+zBearzz@users.noreply.github.com>
2023-03-17 09:29:31 +0100

    New sachen

--- Form1.cs
+++ Form1.cs
@@ -13,9 +13,16 @@ namespace Alles_wiederholen
 {
     public partial class Form1 : Form
     {
+        Kontodaten konto = new Kontodaten();
+        bool loggi = false;
+
+
+
+
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,8 +31,26 @@ namespace Alles_wiederholen
             OleDbCommand cmd = new OleDbCommand();
             con.Open();
             cmd.Connection = con;
-            cmd.CommandText = "select * from Mitarbeiter where Vorname='"+ txt_box_vorname +"' and Nachname='"+ txt_box_nachname +"' and Kennwort='"+ txt_box_kennwort +"';";
+            cmd.CommandText = "select * from Mitarbeiter where Vorname='"+ txt_box_vorname.Text +"' and Nachname='"+ txt_box_nachname.Text +"' and Kennwort='"+ txt_box_kennwort.Text +"';";
+            OleDbDataReader reader = cmd.ExecuteReader();
 
+            while (reader.Read())
+            {
+                if (reader["Vorname"].ToString() == txt_box_vorname.Text && reader["Nachname"].ToString() == txt_box_nachname.Text && reader["Kennwort"].ToString() == txt_box_kennwort.Text)
+                {
+                    konto.info(Convert.ToInt32(reader.GetValue(0).ToString()), reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), reader.GetValue(3).ToString(), reader.GetValue(4).ToString());
+                    loggi = true;
+                }
+            }
+
+
+
+            reader.Close();
+            con.Close();
+            if (loggi)
+            {
+                MessageBox.Show("Loggi ich bin Floggi");
+            }
         }
     }
 }
--- Kontodaten.cs
+++ Kontodaten.cs
@@ -12,11 +12,19 @@ namespace Alles_wiederholen
         public string Nachname { get; set; }
         public string Vorname { get; set; }
         public string Kennung { get; set; }
-        public string Passwort { get; set; }
+        public string Kennwort { get; set; }
 
         public Kontodaten()
         {
 
+        }
+        public void info(int MaNr, string Nachname, string Vorname, string Kennung, string Kennwort)
+        {
+            this.MaNr = MaNr;
+            this.Nachname = Nachname;
+            this.Vorname = Vorname;
+            this.Kennung = Kennung;
+            this.Kennwort = Kennwort;
         }
 
     }
